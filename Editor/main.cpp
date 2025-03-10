c9aaa927868d32ecad6c0cf969b3e2358e7e6202
Aodrenn LE TOURNEUR HUGON <a.letourneurhugon@student.isartdigital.com>
2025-03-10 11:58:48 +0100

    feat(wip): Wrapper start

--- Editor/main.cpp
+++ Editor/main.cpp
@@ -1,4 +1,11 @@
+
+
+#include <iostream>
+#include "../Engine/Code/Include/Wrapper/IWindow.h"
+
 int main()
 {
+    //IWindow t_window = IWindow();
+    std::cout << "test" << '\n';
     return 0;
 }
