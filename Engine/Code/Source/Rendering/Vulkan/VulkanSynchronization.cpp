7a2815e7e26d5957875cb6a73958d18c407893d1
l.yame <l.yame@student.isartdigital.com>
2025-03-28 17:26:49 +0100

    fix : Destroy Vulkan Synchronization

--- Engine/Code/Source/Rendering/Vulkan/VulkanSynchronization.cpp
+++ Engine/Code/Source/Rendering/Vulkan/VulkanSynchronization.cpp
@@ -35,6 +35,8 @@ void VulkanSynchronization::Create(IDevice* a_device)
 
 void VulkanSynchronization::Destroy(IDevice* a_device)
 {
+	vkQueueWaitIdle(a_device->CastVulkan()->GetGraphicsQueue());
+
 	for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; ++i) {
 
 		vkDestroySemaphore(a_device->CastVulkan()->GetDevice(), m_renderFinishedSemaphores[i], nullptr);
