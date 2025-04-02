6fba9a1122ee72d6a1bb39fcd57cb6a2dcac5866
Ethan Ruhlmann <e.ruhlmann@student.isartdigital.com>
2025-04-02 20:08:09 +0200

    refactor: Cleanup VulkanRenderPass

--- Engine/Code/Include/Rendering/Vulkan/VulkanRenderPass.hpp
+++ Engine/Code/Include/Rendering/Vulkan/VulkanRenderPass.hpp
@@ -14,13 +14,16 @@ public :
     void CreateRenderPass(ISwapChain* a_swapChain, IDevice* a_device);
 
     VkFormat FindDepthFormat(VkPhysicalDevice a_physicalDevice);
-
     VkFormat FindSupportedFormat(VkPhysicalDevice a_physicalDevice, const std::vector<VkFormat>& a_candidates, VkImageTiling a_tiling, VkFormatFeatureFlags a_features);
-
     VulkanRenderPass* CastVulkan() override { return this; }
-
-    [[nodiscard]] virtual VkRenderPass GetRenderPass() const { return m_renderPass; }
+    [[nodiscard]] VkRenderPass GetRenderPass() const { return m_renderPass; }
 
 private:
+    void FillColorAttachmentInfo(VkAttachmentDescription& a_colorAttachment, const VkFormat& a_swapChainImageFormat);
+    void FillDepthAttachmentInfo(VkAttachmentDescription& a_depthAttachment, const VkFormat& a_depthFormat);
+    void SetupSubpass(VkSubpassDescription& a_subpass, const VkAttachmentReference& a_colorAttachmentReference, const VkAttachmentReference& a_depthAttachmentReference);
+    void SetupSubpassDependency(VkSubpassDependency& a_dependency);
+    void SetupRenderPassCreateInfo(VkRenderPassCreateInfo& a_renderPassCreateInfo, const std::array<VkAttachmentDescription, 2>& a_attachments, const VkSubpassDescription& a_subpass, const VkSubpassDependency& a_dependency);
+
     VkRenderPass m_renderPass = nullptr;
 };
