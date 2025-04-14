0349eb4ea2cc530f978b6bf5fad82c907ebfaadd
Maxence Lieval <m.lieval@student.isartdigital.com>
2025-04-14 15:11:34 +0200

    Fix: Change Menu into MainMenuBar

--- Editor/Code/Source/FoldersWindow.cpp
+++ Editor/Code/Source/FoldersWindow.cpp
@@ -3,23 +3,23 @@
 
 void FoldersWindow::Draw()
 {
-	ImGui::PushStyleColor(ImGuiCol_WindowBg, 0xff323432);
-	ImGui::SetNextWindowSize({ 1920, 215 }, ImGuiCond_FirstUseEver);
+    ImGui::PushStyleColor(ImGuiCol_WindowBg, 0xff323432);
+    ImGui::SetNextWindowSize({ 1920, 215 }, ImGuiCond_FirstUseEver);
 
-	if (m_isOpen && ImGui::Begin("Folder", &m_isOpen, ImGuiWindowFlags_NoCollapse))
-	{
-		if (ImGui::BeginTable("table1", 3, ImGuiTableFlags_BordersInnerH | ImGuiTableFlags_BordersInnerV | ImGuiTableFlags_BordersOuterH | ImGuiTableFlags_BordersOuterV, { 0, 0 }))
-		{
-			ImGui::TableSetupColumn("Name", ImGuiTableColumnFlags_None, 0);
-			ImGui::TableSetupColumn("Size", ImGuiTableColumnFlags_None, 0);
-			ImGui::TableSetupColumn("Type", ImGuiTableColumnFlags_None, 0);
-			ImGui::TableHeadersRow();
-			ImGui::TableNextRow(0, 150);
-			ImGui::TableSetColumnIndex(0);
+    if (m_isOpen && ImGui::Begin("Folder", &m_isOpen, ImGuiWindowFlags_NoCollapse))
+    {
+        if (ImGui::BeginTable("table1", 3, ImGuiTableFlags_BordersInnerH | ImGuiTableFlags_BordersInnerV | ImGuiTableFlags_BordersOuterH | ImGuiTableFlags_BordersOuterV, { 0, 0 }))
+        {
+            ImGui::TableSetupColumn("Name", ImGuiTableColumnFlags_None, 0);
+            ImGui::TableSetupColumn("Size", ImGuiTableColumnFlags_None, 0);
+            ImGui::TableSetupColumn("Type", ImGuiTableColumnFlags_None, 0);
+            ImGui::TableHeadersRow();
+            ImGui::TableNextRow(0, 150);
+            ImGui::TableSetColumnIndex(0);
 
-			ImGui::EndTable();
-		}
-		ImGui::End();
-		ImGui::PopStyleColor();
-	}
+            ImGui::EndTable();
+        }
+    }
+    ImGui::End();
+    ImGui::PopStyleColor();
 }
--- Editor/Code/Source/MainWindow.cpp
+++ Editor/Code/Source/MainWindow.cpp
@@ -2,125 +2,121 @@
 
 #include "imgui.h"
 
- void MainWindow::Draw()
+void MainWindow::Draw()
 {
-	ImGui::PushStyleColor(ImGuiCol_WindowBg, 0xff323432);
-	ImGui::SetNextWindowSize({ 1920, 1080 }, ImGuiCond_FirstUseEver);
+    ImGui::PushStyleColor(ImGuiCol_WindowBg, 0xff323432);
+    ImGui::SetNextWindowSize({ 1920, 1080 }, ImGuiCond_FirstUseEver);
 
-	if (m_isOpen && ImGui::Begin("Luminous", &m_isOpen, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_MenuBar))
-	{
-		// TODO: Add Draw calls of dependent popup windows here
-		if (ImGui::BeginMenuBar())
-		{
-			if (ImGui::BeginMenu("File"))
-			{
-				ImGui::MenuItem("New Project", "", false);
+    // TODO: Add Draw calls of dependent popup windows here
+    if (ImGui::BeginMainMenuBar())
+    {
+        if (ImGui::BeginMenu("File"))
+        {
+            ImGui::MenuItem("New Project", "", false);
 
-				ImGui::MenuItem("Open project ...", "", false);
+            ImGui::MenuItem("Open project ...", "", false);
 
-				if (ImGui::BeginMenu("Open recent Project"))
-				{
-					ImGui::MenuItem("Project1", "", false);
+            if (ImGui::BeginMenu("Open recent Project"))
+            {
+                ImGui::MenuItem("Project1", "", false);
 
-					ImGui::MenuItem("Project2", "", false);
+                ImGui::MenuItem("Project2", "", false);
 
-					ImGui::EndMenu();
-				}
-				ImGui::Separator();
-				ImGui::MenuItem("Save all", "", false);
+                ImGui::EndMenu();
+            }
+            ImGui::Separator();
+            ImGui::MenuItem("Save all", "", false);
 
-				ImGui::MenuItem("Save all as ...", "", false);
+            ImGui::MenuItem("Save all as ...", "", false);
 
-				ImGui::Separator();
-				ImGui::MenuItem("Build Project", "", false);
+            ImGui::Separator();
+            ImGui::MenuItem("Build Project", "", false);
 
-				ImGui::MenuItem("Build Project and Run", "", false);
+            ImGui::MenuItem("Build Project and Run", "", false);
 
-				ImGui::Separator();
-				ImGui::MenuItem("Exit", "", false);
+            ImGui::Separator();
+            ImGui::MenuItem("Exit", "", false);
 
-				ImGui::EndMenu();
-			}
+            ImGui::EndMenu();
+        }
 
-			if (ImGui::BeginMenu("Edit"))
-			{
+        if (ImGui::BeginMenu("Edit"))
+        {
 
-				ImGui::MenuItem("Undo", "", false);
+            ImGui::MenuItem("Undo", "", false);
 
-				ImGui::MenuItem("Redo", "", false);
+            ImGui::MenuItem("Redo", "", false);
 
-				ImGui::Separator();
-				ImGui::MenuItem("Cut", "", false);
+            ImGui::Separator();
+            ImGui::MenuItem("Cut", "", false);
 
-				ImGui::MenuItem("Copy", "", false);
+            ImGui::MenuItem("Copy", "", false);
 
-				ImGui::MenuItem("Paste", "", false);
+            ImGui::MenuItem("Paste", "", false);
 
-				ImGui::MenuItem("Duplicate", "", false);
+            ImGui::MenuItem("Duplicate", "", false);
 
-				ImGui::MenuItem("Delete", "", false);
+            ImGui::MenuItem("Delete", "", false);
 
-				ImGui::Separator();
-				ImGui::MenuItem("Settings", "", false);
+            ImGui::Separator();
+            ImGui::MenuItem("Settings", "", false);
 
-				ImGui::EndMenu();
-			}
+            ImGui::EndMenu();
+        }
 
-			if (ImGui::BeginMenu("Window"))
-			{
-				ImGui::MenuItem("Console", "", false);
+        if (ImGui::BeginMenu("Window"))
+        {
+            ImGui::MenuItem("Console", "", false);
 
-				ImGui::MenuItem("Scene", "", false);
+            ImGui::MenuItem("Scene", "", false);
 
-				ImGui::MenuItem("Game", "", false);
+            ImGui::MenuItem("Game", "", false);
 
-				ImGui::EndMenu();
-			}
+            ImGui::EndMenu();
+        }
 
-			if (ImGui::BeginMenu("Help"))
-			{
-				ImGui::MenuItem("About", "", false);
+        if (ImGui::BeginMenu("Help"))
+        {
+            ImGui::MenuItem("About", "", false);
 
-				ImGui::Separator();
-				ImGui::MenuItem("Luminous Engine Docmentation", "", false);
-				
-				ImGui::MenuItem("Credits", "", false);
+            ImGui::Separator();
+            ImGui::MenuItem("Luminous Engine Docmentation", "", false);
 
-				ImGui::MenuItem("Report a bug", "", false);
+            ImGui::MenuItem("Credits", "", false);
 
-				ImGui::EndMenu();
-			}
-			ImGui::EndMenuBar();
-		}
+            ImGui::MenuItem("Report a bug", "", false);
 
-		/*static ImTextureID s_moveButton = LoadTexture("../../Assets/Move Button.png");
-		static ImTextureID s_rotateButton = LoadTexture("../../Assets/Rotate Button.png");
-		static ImTextureID s_resizeButton = LoadTexture("../../Assets/Resize Button.png");
-		static ImTextureID s_playButton = LoadTexture("../../Assets/Play Button.png");
-		static ImTextureID s_stopButton = LoadTexture("../../Assets/Stop Button.png");
-		static ImTextureID s_saveButton = LoadTexture("../../Assets/Save Button.png");
+            ImGui::EndMenu();
+        }
+        ImGui::EndMainMenuBar();
+    }
 
-		ImGui::Image(s_moveButton, { 25, 25 }, { 0, 0 }, { 1, 1 });
-		//if (ImGui::IsItemClicked()) 
-			//#Fonction;
-		//if (ImGui::IsItemActivated())
-			//#Fonction;
+    /*static ImTextureID s_moveButton = LoadTexture("../../Assets/Move Button.png");
+    static ImTextureID s_rotateButton = LoadTexture("../../Assets/Rotate Button.png");
+    static ImTextureID s_resizeButton = LoadTexture("../../Assets/Resize Button.png");
+    static ImTextureID s_playButton = LoadTexture("../../Assets/Play Button.png");
+    static ImTextureID s_stopButton = LoadTexture("../../Assets/Stop Button.png");
+    static ImTextureID s_saveButton = LoadTexture("../../Assets/Save Button.png");
 
-		ImGui::SameLine();
-		ImGui::Image(s_rotateButton, { 25, 25 }, { 0, 0 }, { 1, 1 });
+    ImGui::Image(s_moveButton, { 25, 25 }, { 0, 0 }, { 1, 1 });
+    //if (ImGui::IsItemClicked())
+        //#Fonction;
+    //if (ImGui::IsItemActivated())
+        //#Fonction;
 
-		ImGui::SameLine();
-		ImGui::Image(s_resizeButton, { 25, 25 }, { 0, 0 }, { 1, 1 });
+    ImGui::SameLine();
+    ImGui::Image(s_rotateButton, { 25, 25 }, { 0, 0 }, { 1, 1 });
 
-		ImGui::SameLine(0, 100 * ImGui::GetStyle().ItemSpacing.x);
-		ImGui::Image(s_playButton, { 25, 25 }, { 0, 0 }, { 1, 1 });
+    ImGui::SameLine();
+    ImGui::Image(s_resizeButton, { 25, 25 }, { 0, 0 }, { 1, 1 });
 
-		ImGui::SameLine();
-		ImGui::Image(s_stopButton, { 25, 25 }, { 0, 0 }, { 1, 1 });
+    ImGui::SameLine(0, 100 * ImGui::GetStyle().ItemSpacing.x);
+    ImGui::Image(s_playButton, { 25, 25 }, { 0, 0 }, { 1, 1 });
 
-		ImGui::SameLine(0, 115 * ImGui::GetStyle().ItemSpacing.x);
-		ImGui::Image(s_saveButton, { 25, 25 }, { 0, 0 }, { 1, 1 });*/
-	}
-	ImGui::End();
-	ImGui::PopStyleColor();
+    ImGui::SameLine();
+    ImGui::Image(s_stopButton, { 25, 25 }, { 0, 0 }, { 1, 1 });
+
+    ImGui::SameLine(0, 115 * ImGui::GetStyle().ItemSpacing.x);
+    ImGui::Image(s_saveButton, { 25, 25 }, { 0, 0 }, { 1, 1 });*/
+    ImGui::PopStyleColor();
 }
