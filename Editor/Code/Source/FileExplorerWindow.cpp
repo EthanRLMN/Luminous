6490c4b39866dbaea9c5b63a715501e6ae09f256
Maxence Lieval <m.lieval@student.isartdigital.com>
2025-04-15 14:32:19 +0200

    Feat(WIP): Display Files in Assets Location

--- Editor/Code/Source/FileExplorerWindow.cpp
+++ Editor/Code/Source/FileExplorerWindow.cpp
@@ -2,24 +2,34 @@
 
 #include "imgui.h"
 
-#include <filesystem>
+
+const char* s_AssetsDirectory = "Engine/Assets";
+    
+void FileExplorerWindow::ContentBrowserPanel() 
+{
+    m_currentDirectory = s_AssetsDirectory;
+}
 
 void FileExplorerWindow::Draw()
 {
     ImGui::SetNextWindowSize({ 1920, 215 }, ImGuiCond_FirstUseEver);
     
-    const char* s_AssetsDirectory = "assets";
 
     if (p_isOpen)
     {
         ImGui::Begin(p_windowIdentifier.c_str(), &p_isOpen, ImGuiWindowFlags_NoCollapse);
         ImGui::PushStyleColor(ImGuiCol_WindowBg, 0xff323432);
 
-        /* for (auto& p : std::filesystem::directory_iterator(s_AssetsDirectory))
+        for (auto& p : std::filesystem::directory_iterator(m_currentDirectory))
         {
             std::string path = p.path().string();
-            ImGui::Text("%s", path.c_str());
-        }*/
+            if (p.is_directory())
+            {
+                if (ImGui::Button(path.c_str()))
+                {
+                }
+            }
+        }
 
         ImGui::PopStyleColor();
         ImGui::End();
