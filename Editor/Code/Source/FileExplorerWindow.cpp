96b3f45c6798fc12149c4051637207effbaeb490
Maxence Lieval <m.lieval@student.isartdigital.com>
2025-04-15 15:00:31 +0200

    Feat(WIP): add Return system to get back from a selected folder

--- Editor/Code/Source/FileExplorerWindow.cpp
+++ Editor/Code/Source/FileExplorerWindow.cpp
@@ -20,13 +20,22 @@ void FileExplorerWindow::Draw()
         ImGui::Begin(p_windowIdentifier.c_str(), &p_isOpen, ImGuiWindowFlags_NoCollapse);
         ImGui::PushStyleColor(ImGuiCol_WindowBg, 0xff323432);
 
-        for (auto& p : std::filesystem::directory_iterator(m_currentDirectory))
+        if (m_currentDirectory != std::filesystem::path(s_AssetsDirectory))
+        {
+            if (ImGui::Button("<-"))
+            {
+                m_currentDirectory = m_currentDirectory.parent_path();
+            }
+        }
+
+        for (auto& p : std::filesystem::directory_iterator(s_AssetsDirectory))
         {
             std::string path = p.path().string();
             if (p.is_directory())
             {
                 if (ImGui::Button(path.c_str()))
                 {
+                    m_currentDirectory /= p.path().filename();
                 }
             }
         }
