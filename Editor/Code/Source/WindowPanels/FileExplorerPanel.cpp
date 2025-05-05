1b2b3f0316dec98f1c4154f6df5f406189e42eb8
Maxence Lieval <m.lieval@student.isartdigital.com>
2025-05-05 10:39:01 +0200

    Feat: Rearrenging Explorer by the size of the window

--- Editor/Code/Source/WindowPanels/FileExplorerPanel.cpp
+++ Editor/Code/Source/WindowPanels/FileExplorerPanel.cpp
@@ -7,7 +7,7 @@
 
 
 static const std::filesystem::path s_AssetPath = "Engine/Assets";
-static const std::filesystem::path s_IconPath = "Editor/Assets/Icons";
+static const std::filesystem::path s_IconPath = "Editor/Assets/Icons/";
 
 
 FileExplorerPanel::FileExplorerPanel(Editor* a_editor, const std::string& a_windowIdentifier) :
@@ -15,8 +15,8 @@ FileExplorerPanel::FileExplorerPanel(Editor* a_editor, const std::string& a_wind
 {
     m_currentDirectory = s_AssetPath;
     m_engine = a_editor->GetEngine();
-    m_directoryIconTexture = LoadTexture(m_engine, "Editor/Assets/Icons/DirectoryIcon.png");
-    m_fileIconTexture = LoadTexture(m_engine, "Editor/Assets/Icons/FileIcon.png");
+    m_directoryIconTexture = LoadTexture(m_engine, (s_IconPath / "DirectoryIcon.png").string());
+    m_fileIconTexture = LoadTexture(m_engine, (s_IconPath / "FileIcon.png").string());
 }
 
 void FileExplorerPanel::Render()
@@ -36,40 +36,52 @@ void FileExplorerPanel::Render()
             }
         }
 
+        float padding = 64.0f;
+        float thumbnailSize = 64.0f;
+        float cellSize = thumbnailSize + padding;
+        float panelWidth = ImGui::GetContentRegionAvail().x;
+        int columnCount = (int) (panelWidth / cellSize);
+        if (columnCount < 1)
+            columnCount = 1;
+
+        ImGui::Columns(columnCount, 0, false);
+
         for (auto& directoryEntry : std::filesystem::directory_iterator(m_currentDirectory))
         {
             const auto& path = directoryEntry.path();
             auto relativePath = std::filesystem::relative(path, s_AssetPath);
             std::string filenameString = relativePath.filename().string();
-            if (directoryEntry.is_directory())
+
+            ImGui::PushID(filenameString.c_str());
+
+            ImTextureID icon = (ImTextureID) (uintptr_t) (directoryEntry.is_directory() ? m_directoryIconTexture->CastVulkan()->GetTextureImageView()
+                                                                                        : m_fileIconTexture->CastVulkan()->GetTextureImageView());
+
+            if (ImGui::ImageButton(filenameString.c_str(), icon, ImVec2(thumbnailSize, thumbnailSize)))
             {
-                ImGui::SameLine();
-                ImGui::PushID(filenameString.c_str());
-                if (ImGui::ImageButton("Folder", (ImTextureID) (uintptr_t) m_directoryIconTexture->CastVulkan()->GetTextureImageView(), ImVec2(64, 64)))
+                if (directoryEntry.is_directory())
                 {
-                    m_currentDirectory /= directoryEntry.path().filename();
-                }
-                ImGui::PopID();
-            } else
-            {
-                ImGui::SameLine();
-                ImGui::PushID(filenameString.c_str());
-                if (ImGui::ImageButton("File", (ImTextureID) (uintptr_t) m_fileIconTexture->CastVulkan()->GetTextureImageView(), ImVec2(64, 64)))
+                    m_currentDirectory /= path.filename();
+                } else
                 {
                     std::string extension = path.extension().string();
-
                     if (extension == ".txt" || extension == ".cpp" || extension == ".hpp")
                     {
                         OpenTextEditor(path);
                     }
                 }
-                ImGui::PopID();
             }
-        }
 
-        ImGui::PopStyleColor();
-        ImGui::End();
+            ImGui::TextWrapped("%s", filenameString.c_str());
+
+            ImGui::NextColumn();
+            ImGui::PopID();
+        }
+        ImGui::Columns(1);
     }
+
+    ImGui::PopStyleColor();
+    ImGui::End();
 }
 
 
