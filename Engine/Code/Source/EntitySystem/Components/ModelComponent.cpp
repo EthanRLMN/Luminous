f1d4e61bf32786f0d602127ea75160ab9735b375
a.letourneurhugon <a.letourneurhugon@student.isartdigital.com>
2025-05-08 13:50:08 +0200

    refactor (wip) : set singular buffers

--- Engine/Code/Source/EntitySystem/Components/ModelComponent.cpp
+++ Engine/Code/Source/EntitySystem/Components/ModelComponent.cpp
@@ -1,5 +1,6 @@
 #include "EntitySystem/Components/ModelComponent.hpp"
 #include "ResourceManager/ResourceManager.hpp"
+#include "Engine.hpp"
 
 
 void ModelComponent::Initialize()
@@ -10,8 +11,8 @@ void ModelComponent::Initialize()
         DEBUG_LOG_ERROR("ENGINE OK !");
     }
 
-    //SetMesh("Engine/Assets/Models/viking_room.obj");
-    //SetTexture("Engine/Assets/Textures/viking_room.png");
+    SetMesh("Engine/Assets/Models/viking_room.obj");
+    SetTexture("Engine/Assets/Textures/viking_room.png");
 }
 
 void ModelComponent::SetMesh(std::string a_path) 
@@ -20,11 +21,13 @@ void ModelComponent::SetMesh(std::string a_path)
     IResourceParams l_meshParams{};
     l_meshParams.m_meshPath = a_path;
     m_mesh = ResourceManager::GetInstance().LoadResource<VulkanMesh>(l_meshParams);
+    m_engine->GetBuffer()->SetMeshBuffers(m_engine->GetDevice(), m_engine->GetSwapChain(), m_engine->GetCommandPool(), m_mesh);
+
 }
 
 void ModelComponent::SetTexture(std::string a_path)
 { 
-    IResourceParams l_texParams{  };
+    IResourceParams l_texParams{ m_engine->GetDevice(), m_engine->GetSwapChain(), m_engine->GetDepthResource(), m_engine->GetCommandPool() };
     l_texParams.m_texturePath = a_path;
     m_texture = ResourceManager::GetInstance().LoadResource<VulkanTexture>(l_texParams);
 }
