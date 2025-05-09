ab096739a4adcc1b747187a613b36639591f4545
Ethan Ruhlmann <e.ruhlmann@student.isartdigital.com>
2025-05-09 13:16:48 +0200

    feat(WIP): Add further work on transform component

--- Engine/Code/Include/Game/Systems/Component/TransformComponent.hpp
+++ Engine/Code/Include/Game/Systems/Component/TransformComponent.hpp
@@ -7,6 +7,7 @@
 #include "Quaternion.hpp"
 
 #include "Vector3.hpp"
+#include "Game/Systems/Entity/Entity.hpp"
 
 class Entity;
 
@@ -25,10 +26,12 @@ public:
     inline Maths::Vector3 GetGlobalScale() const { return m_globalScale; }
     inline Maths::Vector3 GetGlobalRotationVec() const { return m_globalRotation.ToEulerAngles(true); }
     inline Maths::Quaternion GetGlobalRotationQuat() const { return m_globalRotation; }
-    inline Maths::Matrix4 GetModel() const { return m_modelMatrix; }
+    inline Maths::Matrix4 GetLocalMatrix() const { return m_localMatrix; }
+    inline Maths::Matrix4 GetGlobalMatrix() const { return m_globalMatrix; }
     inline std::shared_ptr<Entity> GetParent() const { return m_parent; }
 
-    inline void SetModel(const Maths::Matrix4& a_modelMatrix) { m_modelMatrix = a_modelMatrix; }
+    inline void SetLocalMatrix(const Maths::Matrix4& a_newMatrix) { m_localMatrix = a_newMatrix; }
+    inline void SetGlobalMatrix(const Maths::Matrix4& a_newMatrix) { m_globalMatrix = a_newMatrix; }
     inline void SetLocalPosition(const Maths::Vector3 a_newPos) { m_localPosition = a_newPos; }
     inline void SetLocalScale(const Maths::Vector3 a_newScale) { m_localScale = a_newScale; }
     inline void SetLocalRotationVec(const Maths::Vector3 a_newRotVec) { m_localRotation = Maths::Quaternion::FromEulerAngles(a_newRotVec); }
@@ -37,8 +40,12 @@ public:
     inline void SetGlobalScale(const Maths::Vector3 a_newScale) { m_globalScale = a_newScale; }
     inline void SetGlobalRotationVec(const Maths::Vector3 a_newRotVec) { m_globalRotation = Maths::Quaternion::FromEulerAngles(a_newRotVec); }
     inline void SetGlobalRotationQuat(const Maths::Quaternion a_newRotQuat) { m_globalRotation = a_newRotQuat; }
-    inline void SetParent(const std::shared_ptr<Entity>& a_newParent) { m_parent = a_newParent ; }
 
+    inline void SetParent(const std::shared_ptr<Entity>& a_newParent)
+    {
+        m_parent = a_newParent;
+        UpdateGlobalTransform();
+    }
 
 
 private:
@@ -51,5 +58,22 @@ private:
     Maths::Quaternion m_globalRotation { Maths::Quaternion::Identity };
     Maths::Vector3 m_globalScale { Maths::Vector3::One };
 
-    Maths::Matrix4 m_modelMatrix { Maths::Matrix4::TRS(m_localPosition, m_localRotation.ToEulerAngles(true), m_localScale) };
+    Maths::Matrix4 m_localMatrix { Maths::Matrix4::TRS(m_localPosition, m_localRotation.ToEulerAngles(true), m_localScale) };
+    Maths::Matrix4 m_globalMatrix { Maths::Matrix4::TRS(m_globalPosition, m_globalRotation.ToEulerAngles(true), m_globalScale) };
+
+    inline void UpdateGlobalTransform()
+    {
+        m_localMatrix = Maths::Matrix4::TRS(m_localPosition, m_localRotation.ToEulerAngles(true), m_localScale);
+
+        if (m_parent)
+        {
+            const std::shared_ptr<TransformComponent> l_parentTransform = m_parent->GetComponent<TransformComponent>();
+            if (l_parentTransform)
+                m_globalMatrix = l_parentTransform->GetGlobalMatrix() * m_localMatrix;
+            else
+                m_globalMatrix = m_localMatrix;
+        }
+        else
+            m_globalMatrix = m_localMatrix;
+    }
 };
