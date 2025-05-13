351fbacb626b5909167bbadc9b4972930897ad07
l.yame <l.yame@student.isartdigital.com>
2025-05-13 13:17:55 +0200

    Feat : test (and of course this isn't working)

--- Engine/Code/Include/Game/Scene.hpp
+++ Engine/Code/Include/Game/Scene.hpp
@@ -2,7 +2,20 @@
 
 #include "Systems/Entity/Entity.hpp"
 
+#include <iostream>
+#include <rfl.hpp>
+#include <rfl/json.hpp>
+#include <string>
+#include <vector>
+
 class Engine;
+
+struct Entity_Saver
+{
+    rfl::Rename<"name", std::string> entityName;
+    uint16_t entityUUID;
+    bool isActive;
+};
 
 class Scene
 {
--- Engine/Code/Source/Game/Scene.cpp
+++ Engine/Code/Source/Game/Scene.cpp
@@ -4,12 +4,33 @@
 #include "Game/Systems/Entity/EntityFactory.hpp"
 #include "Game/Systems/Entity/EntityTemplates.hpp"
 
+
+
 void Scene::RegisterScene(EntityManager& a_entityManager)
 {
+    const rfl::Result<Entity_Saver> result = rfl::json::load<Entity_Saver>("Engine/Assets/Default/Save/Scene.json");
+
+    const std::string filepath = "Engine/Assets/Default/Save/Scene.json";
+
+    if (!std::filesystem::exists(filepath)) 
+    {
+        DEBUG_LOG_CRITICAL("JSon Not find = {}", filepath);
+    } 
+    else
+    {
+        DEBUG_LOG_INFO("Load File Json");
+    }
+
+
     EntityTemplates l_defaultTemplates {};
     l_defaultTemplates.RegisterEntityTemplates();
 
     a_entityManager.CreateEntityFromTemplate("Companion");
+
+
+    Entity_Saver entitySaver; 
+
+
 
 
     for (const auto& l_entity : EntityManager::GetAvailableTemplates())
@@ -19,6 +40,8 @@ void Scene::RegisterScene(EntityManager& a_entityManager)
 
     for (const auto& l_entity : a_entityManager.GetEntities())
     {
-        DEBUG_LOG_CRITICAL("ENTITY = {}", l_entity->GetUUID());
+        //DEBUG_LOG_CRITICAL("ENTITY = {}", l_entity->GetUUID());
+        l_entity->SetActive(entitySaver.isActive);
+        DEBUG_LOG_CRITICAL("ENTITY is active ? = {}",l_entity->IsActive());
     }
 }
