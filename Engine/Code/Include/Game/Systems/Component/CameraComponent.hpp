3990d7700f2563734d8e35ccbc0bd2f537b2fabc
l.yame <l.yame@student.isartdigital.com>
2025-05-15 12:05:14 +0200

    feat : camera component init

--- Engine/Code/Include/Game/Systems/Component/CameraComponent.hpp
+++ Engine/Code/Include/Game/Systems/Component/CameraComponent.hpp
@@ -7,4 +7,20 @@ class CameraComponent : public Camera
 public:
     CameraComponent() = default;
 
+    bool GetisActive() { return m_isActive; }
+    float GetNearPlane() { return m_nearPlane; }
+    float GetFarPlane() { return m_farPlane; }
+    float GetFieldOfView() { return m_fieldOfView; }
+
+    void SetIsActive(bool a_isActive) { m_isActive = a_isActive; }
+    void SetNearPlane(float a_nearPlane) { m_nearPlane = a_nearPlane; }
+    void SetFarPlane(float a_farPlane) { m_farPlane = a_farPlane; }
+    void SetFieldOfView(float a_fieldofView) { m_fieldOfView = a_fieldofView; }
+
+private: 
+
+    bool m_isActive;
+    float m_nearPlane;
+    float m_farPlane;
+    float m_fieldOfView
 };
