1f2abf7db848357f5130afd882585ed641fb3992
l.yame <l.yame@student.isartdigital.com>
2025-05-15 13:53:37 +0200

    feat : add camera copomponent

--- Engine/Code/Include/Game/Systems/Component/CameraComponent.hpp
+++ Engine/Code/Include/Game/Systems/Component/CameraComponent.hpp
@@ -1,11 +1,16 @@
 #pragma once
-
+#include "EntityComponent.hpp"
 #include "Game/Systems/Camera/Camera.hpp"
 
-class CameraComponent : public Camera
+class CameraComponent : public Camera ,public EntityComponent
 {
 public:
     CameraComponent() = default;
+
+    void Initialize() override;
+    void GameplayStarted() override ;
+    void Update() override ;
+  
 
     bool GetisActive() { return m_isActive; }
     float GetNearPlane() { return m_nearPlane; }
@@ -17,10 +22,20 @@ public:
     void SetFarPlane(float a_farPlane) { m_farPlane = a_farPlane; }
     void SetFieldOfView(float a_fieldofView) { m_fieldOfView = a_fieldofView; }
 
-private: 
-
+private:
     bool m_isActive;
-    float m_nearPlane;
-    float m_farPlane;
-    float m_fieldOfView
+    float m_aspectRatio{ 800.0f / 600.0f };
+    float m_nearPlane{ 0.1f };
+    float m_farPlane{ 100.f };
+    float m_fieldOfView{ 60.0f };
+
+
+    Maths::Matrix4 m_projectionMatrix{ Maths::Matrix4::identity };
+    Maths::Matrix4 m_viewMatrix{ Maths::Matrix4::identity };
+    Maths::Vector3 m_eye{ 0.0f, 0.0f, 5.0f };
+    Maths::Vector3 m_center{ 0.0f, 0.0f, 4.0f };
+
+    Maths::Vector3 m_worldRight{ Maths::Vector3::XAxis };
+    Maths::Vector3 m_worldUp{ Maths::Vector3::YAxis };
+    Maths::Vector3 m_worldForward{ Maths::Vector3::ZAxis };
 };
---
+++ Engine/Code/Source/Game/Systems/Component/CameraComponent.cpp
@@ -0,0 +1,19 @@
+#include "Game/Systems/Component/CameraComponent.hpp"
+
+void CameraComponent::Initialize() {
+
+    m_worldForward = (m_center - m_eye).Normalize();
+    m_worldRight = m_worldForward.CrossProduct(m_worldUp).Normalize();
+    m_worldUp = m_worldRight.CrossProduct(m_worldForward).Normalize();
+
+    m_projectionMatrix = UpdateProjectionMatrix(m_fieldOfView, m_aspectRatio, m_nearPlane, m_farPlane);
+}
+
+void CameraComponent::GameplayStarted() {
+
+}
+
+void CameraComponent::Update() {
+    m_viewMatrix = UpdateViewMatrix(m_eye, m_center, m_worldUp);
+}
+
