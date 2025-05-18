731b5826cea7d1a025cf7452ff668214d2441255
Ethan Ruhlmann <e.ruhlmann@student.isartdigital.com>
2025-05-18 18:14:47 +0200

    refactor: Cleanup and optimize gizmo code.

--- Editor/Code/Source/WindowPanels/ViewportPanel.cpp
+++ Editor/Code/Source/WindowPanels/ViewportPanel.cpp
@@ -9,6 +9,14 @@
 
 static const std::filesystem::path s_IconPath = "Editor/Assets/Icons/";
 
+
+void Viewport::Init()
+{
+    InitIcons();
+    dSets = reinterpret_cast<ImTextureID>(ImGui_ImplVulkan_AddTexture(ResourceManager::GetInstance().GetStandardSampler(), p_editor->GetEngine()->GetRenderer()->CastVulkan()->GetViewportImageView(), VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL));
+}
+
+
 void Viewport::Render()
 {
     m_camera = p_editor->GetEngine()->GetRenderer()->CastVulkan()->GetCamera();
@@ -17,127 +25,14 @@ void Viewport::Render()
     ImGui::Begin(p_windowIdentifier.c_str(), nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoBackground);
     ImGuizmo::BeginFrame();
 
-    ImVec2 avail = ImGui::GetContentRegionAvail();
-    float buttonWidth = 40.f;
-    float buttonHeight = 40.f;
-    ImVec2 buttonSize(buttonWidth, buttonHeight);
-
-    if (ImGui::ImageButton("Move", m_iconMoveID, buttonSize))
-    {
-        m_currentGizmoOperation = ImGuizmo::TRANSLATE;
-    }
-    ImGui::SameLine();
-    if (ImGui::ImageButton("Rotate", m_iconRotateID, buttonSize))
-    {
-        m_currentGizmoOperation = ImGuizmo::ROTATE;
-    }
-    ImGui::SameLine();
-    if (ImGui::ImageButton("Resize", m_iconResizeID, buttonSize))
-    {
-        m_currentGizmoOperation = ImGuizmo::SCALE;
-    }
-
-    ImGui::SameLine(0, 0.4f * avail.x);
-
-    if (ImGui::ImageButton("Play", m_iconPlayID, buttonSize))
-    {
-    }
-    ImGui::SameLine();
-    if (ImGui::ImageButton("Stop", m_iconStopID, buttonSize))
-    {
-    }
-
-    if (p_editor->GetEngine()->GetRenderer()->CastVulkan()->m_recreateDsets)
-    {
-        p_editor->GetEngine()->GetRenderer()->CastVulkan()->m_recreateDsets = false;
-        dSets = reinterpret_cast<ImTextureID>(ImGui_ImplVulkan_AddTexture(ResourceManager::GetInstance().GetStandardSampler(), p_editor->GetEngine()->GetRenderer()->CastVulkan()->GetViewportImageView(), VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL));
-    };
-
-    VkExtent2D l_extent = p_editor->GetEngine()->GetSwapChain()->CastVulkan()->GetSwapChainExtent();
-
-    float l_texWidth = static_cast<float>(l_extent.width);
-    float l_texHeight = static_cast<float>(l_extent.height);
-    float l_aspectRatio = l_texWidth / l_texHeight;
-
-    ImVec2 l_avail = ImGui::GetContentRegionAvail();
-    float l_fitWidth = l_avail.x;
-    float l_fitHeight = l_fitWidth / l_aspectRatio;
-
-    if (l_fitHeight > l_avail.y)
-    {
-        l_fitHeight = l_avail.y;
-        l_fitWidth = l_fitHeight * l_aspectRatio;
-    }
-
-    const ImVec2 l_imageSize(l_fitWidth, l_fitHeight);
-
-    const ImVec2 l_screenPos = ImGui::GetCursorScreenPos();
-    const float l_offsetX = (l_avail.x - l_imageSize.x) * 0.5f;
-    const float l_offsetY = (l_avail.y - l_imageSize.y) * 0.5f;
-
-    ImGui::SetCursorScreenPos(ImVec2(l_screenPos.x + l_offsetX, l_screenPos.y + l_offsetY));
-    ImGui::Image(dSets, l_imageSize);
-
-    Maths::Matrix4 view = m_camera.GetViewMatrix().Transpose();
-    Maths::Matrix4 projection = m_camera.GetProjectionMatrix();
-
-    const auto selectedEntity = p_editor->GetSelectedEntity();
-
-    if (selectedEntity)
-    {
-        std::shared_ptr<TransformComponent> transform = selectedEntity->GetComponent<TransformComponent>();
-        if (transform)
-        {
-            Maths::Matrix4 model = transform->GetGlobalMatrix().Transpose();
-            float modelMatrix[16];
-            memcpy(modelMatrix, &model.mat, sizeof(float) * 16);
-
-            ImGuizmo::SetOrthographic(false);
-            ImGuizmo::Enable(true);
-            ImGuizmo::SetDrawlist();
-
-            ImVec2 winPos = ImGui::GetWindowViewport()->Pos;
-
-            ImGuizmo::SetRect(winPos.x + l_offsetX, winPos.y + l_offsetY, l_imageSize.x, l_imageSize.y);
-
-
-            float viewMatrix[16];
-            memcpy(viewMatrix, &view.mat, sizeof(float) * 16);
-
-            float projMatrix[16];
-            memcpy(projMatrix, &projection.mat, sizeof(float) * 16);
-            
-            ImGuizmo::Manipulate(
-                    viewMatrix,
-                    projMatrix,
-                    m_currentGizmoOperation,
-                    ImGuizmo::LOCAL,
-                    modelMatrix);
-
-
-            if (ImGuizmo::IsUsing())
-            {
-                // Convert back to Matrix4
-                Maths::Matrix4 newModelMatrix;
-                memcpy(&newModelMatrix.mat, modelMatrix, sizeof(float) * 16);
-
-                // Transpose back to column-major if needed
-                newModelMatrix = newModelMatrix.Transpose();
-
-                // Décomposer la matrice en position / rotation / scale
-                Maths::Vector3 translation, scale;
-                Maths::Quaternion rotation;
-                newModelMatrix.Decompose(translation, rotation, scale);
-
-                transform->SetGlobalPosition(translation);
-                transform->SetGlobalRotationQuat(rotation); // ou quaternion si besoin
-                transform->SetGlobalScale(scale);
-            }
-        }
-    }
+    RenderToolbarButtons();
+    UpdateTextureIfNeeded();
+    const ViewportImageInfo l_imageInfo = RenderViewportImage();
+    RenderGizmo(l_imageInfo);
 
     ImGui::End();
 }
+
 
 void Viewport::InitIcons()
 {
@@ -163,6 +58,7 @@ void Viewport::InitIcons()
     loadImg(m_iconStop, m_iconStopID);
 }
 
+
 std::shared_ptr<ITexture> Viewport::LoadTexture(const Engine* engine, const std::string& path)
 {
     IResourceParams params{};
@@ -173,4 +69,132 @@ std::shared_ptr<ITexture> Viewport::LoadTexture(const Engine* engine, const std:
     auto texture = std::make_shared<VulkanTexture>();
     texture->CreateTextureImage(params.m_device, params.m_commandPool, params.m_texturePath);
     return texture;
+}
+
+
+void Viewport::RenderToolbarButtons()
+{
+    const ImVec2 l_avail = ImGui::GetContentRegionAvail();
+    constexpr float l_buttonWidth = 40.0f;
+    constexpr float l_buttonHeight = 40.0f;
+    constexpr ImVec2 l_buttonSize(l_buttonWidth, l_buttonHeight);
+
+    if (ImGui::ImageButton("Move", m_iconMoveID, l_buttonSize))
+        m_currentGizmoOperation = ImGuizmo::TRANSLATE;
+
+    ImGui::SameLine();
+    if (ImGui::ImageButton("Rotate", m_iconRotateID, l_buttonSize))
+        m_currentGizmoOperation = ImGuizmo::ROTATE;
+
+    ImGui::SameLine();
+    if (ImGui::ImageButton("Resize", m_iconResizeID, l_buttonSize))
+        m_currentGizmoOperation = ImGuizmo::SCALE;
+
+    ImGui::SameLine(0, 0.4f * l_avail.x);
+
+    if (ImGui::ImageButton("Play", m_iconPlayID, l_buttonSize))
+        { /* TODO: Action */ }
+
+    ImGui::SameLine();
+    if (ImGui::ImageButton("Stop", m_iconStopID, l_buttonSize))
+        { /* TODO: Action */ }
+}
+
+
+void Viewport::UpdateTextureIfNeeded()
+{
+    VulkanRenderer* l_renderer = p_editor->GetEngine()->GetRenderer()->CastVulkan();
+    if (l_renderer->m_recreateDsets)
+    {
+        l_renderer->m_recreateDsets = false;
+
+        dSets = reinterpret_cast<ImTextureID>(ImGui_ImplVulkan_AddTexture(
+            ResourceManager::GetInstance().GetStandardSampler(),
+            l_renderer->GetViewportImageView(),
+            VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL));
+    }
+}
+
+
+void Viewport::RenderGizmo(const ViewportImageInfo& a_imageInfo) const
+{
+    const std::shared_ptr<Entity> l_selectedEntity = p_editor->GetSelectedEntity();
+    if (!l_selectedEntity)
+        return;
+
+    const std::shared_ptr<TransformComponent> l_transform = l_selectedEntity->GetComponent<TransformComponent>();
+    if (!l_transform)
+        return;
+
+    const Maths::Matrix4 l_model = l_transform->GetGlobalMatrix().Transpose();
+    const Maths::Matrix4 l_view = m_camera.GetViewMatrix().Transpose();
+    const Maths::Matrix4 l_projection = m_camera.GetProjectionMatrix();
+
+    float l_modelMatrix[16];
+    float l_viewMatrix[16];
+    float l_projMatrix[16];
+
+    memcpy(l_modelMatrix, &l_model.mat, sizeof(float) * 16);
+    memcpy(l_viewMatrix, &l_view.mat, sizeof(float) * 16);
+    memcpy(l_projMatrix, &l_projection.mat, sizeof(float) * 16);
+
+    ImGuizmo::SetOrthographic(false);
+    ImGuizmo::Enable(true);
+    ImGuizmo::SetDrawlist();
+
+    ImGuizmo::SetRect(a_imageInfo.m_position.x, a_imageInfo.m_position.y, a_imageInfo.m_size.x, a_imageInfo.m_size.y);
+
+    ImGuizmo::Manipulate(
+        l_viewMatrix,
+        l_projMatrix,
+        m_currentGizmoOperation,
+        ImGuizmo::LOCAL,
+        l_modelMatrix);
+
+    if (ImGuizmo::IsUsing())
+    {
+        Maths::Matrix4 l_newModelMatrix;
+        memcpy(&l_newModelMatrix.mat, l_modelMatrix, sizeof(float) * 16);
+        l_newModelMatrix = l_newModelMatrix.Transpose();
+
+        Maths::Vector3 l_translation, l_scale;
+        Maths::Quaternion l_rotation;
+        l_newModelMatrix.Decompose(l_translation, l_rotation, l_scale);
+
+        l_transform->SetGlobalPosition(l_translation);
+        l_transform->SetGlobalRotationQuat(l_rotation);
+        l_transform->SetGlobalScale(l_scale);
+    }
+}
+
+
+Viewport::ViewportImageInfo Viewport::RenderViewportImage() const
+{
+    const VkExtent2D l_extent = p_editor->GetEngine()->GetSwapChain()->CastVulkan()->GetSwapChainExtent();
+
+    const float l_texWidth = static_cast<float>(l_extent.width);
+    const float l_texHeight = static_cast<float>(l_extent.height);
+    const float l_aspectRatio = l_texWidth / l_texHeight;
+
+    const ImVec2 l_avail = ImGui::GetContentRegionAvail();
+    float l_fitWidth = l_avail.x;
+    float l_fitHeight = l_fitWidth / l_aspectRatio;
+
+    if (l_fitHeight > l_avail.y)
+    {
+        l_fitHeight = l_avail.y;
+        l_fitWidth = l_fitHeight * l_aspectRatio;
+    }
+
+    const ImVec2 l_imageSize(l_fitWidth, l_fitHeight);
+    const ImVec2 l_screenPos = ImGui::GetCursorScreenPos();
+
+    const float l_offsetX = (l_avail.x - l_imageSize.x) * 0.5f;
+    const float l_offsetY = (l_avail.y - l_imageSize.y) * 0.5f;
+
+    const ImVec2 l_finalPos(l_screenPos.x + l_offsetX, l_screenPos.y + l_offsetY);
+    ImGui::SetCursorScreenPos(l_finalPos);
+    ImGui::Image(dSets, l_imageSize);
+
+    return { l_finalPos, l_imageSize };
 }
